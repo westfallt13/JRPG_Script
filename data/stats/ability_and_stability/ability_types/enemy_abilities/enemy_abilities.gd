72807673efc8c7b38b2848246bc54aa7de38513d
westfallt13 <westfallt13@gmail.com>
2026-06-18 13:50:52 -0500

    Added guides and fleshed out basic scheme definitions

---
+++ data/stats/ability_and_stability/ability_types/enemy_abilities/enemy_abilities.gd
@@ -0,0 +1,13 @@
+var enemy_abilities : Dictionary {
+    "can_be_used_by" : false, # Placeholder field for potential future use (e.g., which enemies can use this ability)
+    "enemy_ability_name" : "Test Enemy Ability",
+    "description" : "This is a test enemy ability.",
+    "enemy_ability_type" : {
+        "physical" : false,
+        "magical" : false,
+        "status_effect" : false,
+        "healing" : false,
+        "buff" : false,
+        "debuff" : false
+    }
+}
---
+++ data/stats/ability_and_stability/ability_types/magic_abilities/magic_abilities.gd
@@ -0,0 +1,14 @@
+var magic_abilities : Dictionary {
+    "magic_ability_name" : "Test Enemy Ability",
+    "description" : "This is a test enemy ability.",
+    "magic_ability_type" : {
+        "fire" : false,
+        "ice" : false,
+        "lightning" : false,
+        "earth" : false,
+        "wind" : false,
+        "water" : false,
+        "dark" : false,
+        "light" : false
+    }
+}
